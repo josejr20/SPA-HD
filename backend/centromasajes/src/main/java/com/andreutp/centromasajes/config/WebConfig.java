721e01724c08b92c61cc604a95e72a8fe2c966d6
josejr20 <jararojasjose604@gmail.com>
2026-02-20 20:06:23 -0500

    Actualizando

--- backend/centromasajes/src/main/java/com/andreutp/centromasajes/config/WebConfig.java
+++ backend/centromasajes/src/main/java/com/andreutp/centromasajes/config/WebConfig.java
@@ -10,7 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // Ajustar el origen
+                .allowedOrigins(
+                    "http://localhost:3000",
+                    "https://sparelax3.netlify.app/"
+                ) // Ajustar el origen
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
--- backend/centromasajes/src/main/java/com/andreutp/centromasajes/security/SecurityConfig.java
+++ backend/centromasajes/src/main/java/com/andreutp/centromasajes/security/SecurityConfig.java
@@ -17,19 +17,17 @@ public class SecurityConfig {
     private final CustomUserDetailsService userDetailsService;
     private final JwtUtil jwtUtil;
 
-
     public SecurityConfig(CustomUserDetailsService userDetailsService, JwtUtil jwtUtil) {
         this.userDetailsService = userDetailsService;
         this.jwtUtil = jwtUtil;
     }
 
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults()) // 👈 activar CORS
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(
-                auth -> auth
+                .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/appointments/**").permitAll()
                         .requestMatchers("/user/**").permitAll()
                         .requestMatchers("/availability/**").permitAll()
@@ -37,11 +35,11 @@ public class SecurityConfig {
                         .requestMatchers("/promotions/**").permitAll()
                         .requestMatchers("/plans/**").permitAll()
                         .requestMatchers(
-                                        "/auth/register/**",
-                                        "/auth/login/**",
-                                        "/auth/forgot-password/**",
-                                        "/auth/reset-password/**"
-                                ).permitAll()
+                                "/auth/register/**",
+                                "/auth/login/**",
+                                "/auth/forgot-password/**",
+                                "/auth/reset-password/**")
+                        .permitAll()
                         .requestMatchers("/services/**").permitAll()
                         .requestMatchers("/upload/**").permitAll()
                         .requestMatchers("/uploads/**").permitAll()
@@ -49,83 +47,98 @@ public class SecurityConfig {
 
                         .requestMatchers("/api/**").permitAll()
                         .requestMatchers("/config/**").permitAll()
-                        //SERVICIOS ENDPOINS
+                        // SERVICIOS ENDPOINS
                         // Solo ADMIN puede crear/actualizar/eliminar servicios
-                        //.requestMatchers("/services/**" ,"/services" ).hasRole("ADMIN")//sigue pidiendo token si aun esta en permitir todo
+                        // .requestMatchers("/services/**" ,"/services" ).hasRole("ADMIN")//sigue
+                        // pidiendo token si aun esta en permitir todo
                         .requestMatchers("/services/**").permitAll()
                         .requestMatchers("/test-email").permitAll()
                         .requestMatchers("/test-excel").permitAll()
                         .requestMatchers("/reports/**").permitAll()
-                        //cambiarlo luego a que solo el administrador pueda
+                        // cambiarlo luego a que solo el administrador pueda
                         // Solo USER puede listar servicios (GET)
-                        //.requestMatchers("/services").hasAnyRole("USER", "ADMIN")
+                        // .requestMatchers("/services").hasAnyRole("USER", "ADMIN")
 
-                        //PLANES
-                        //.requestMatchers(HttpMethod.GET, "/plans/**").hasAnyRole("ADMIN", "WORKER", "USER") // todos pueden ver
-                        //.requestMatchers("/plans/**").hasAnyRole("ADMIN", "WORKER") // crear/editar/eliminar solo WORKER y ADMIN segun necesidad xd
+                        // PLANES
+                        // .requestMatchers(HttpMethod.GET, "/plans/**").hasAnyRole("ADMIN", "WORKER",
+                        // "USER") // todos pueden ver
+                        // .requestMatchers("/plans/**").hasAnyRole("ADMIN", "WORKER") //
+                        // crear/editar/eliminar solo WORKER y ADMIN segun necesidad xd
 
                         // APPOINTMENTS
                         // Crear cita: solo USER
-                        //.requestMatchers(HttpMethod.POST, "/appointments/**").hasRole("USER")
+                        // .requestMatchers(HttpMethod.POST, "/appointments/**").hasRole("USER")
 
                         // Listar todas las citas: solo ADMIN
-                        //.requestMatchers(HttpMethod.GET, "/appointments").hasRole("ADMIN")
+                        // .requestMatchers(HttpMethod.GET, "/appointments").hasRole("ADMIN")
 
                         // Ver citas de un trabajador: solo WORKER
-                        //.requestMatchers(HttpMethod.GET, "/appointments/worker/**").hasRole("WORKER")
+                        // .requestMatchers(HttpMethod.GET, "/appointments/worker/**").hasRole("WORKER")
 
                         // Ver mis citas: USER
-                        //.requestMatchers(HttpMethod.GET, "/appointments/my/**").hasRole("USER")
+                        // .requestMatchers(HttpMethod.GET, "/appointments/my/**").hasRole("USER")
 
                         // Actualizar estado de cita: WORKER
-                        //.requestMatchers(HttpMethod.PATCH, "/appointments/**/status").hasRole("WORKER")
+                        // .requestMatchers(HttpMethod.PATCH,
+                        // "/appointments/**/status").hasRole("WORKER")
 
                         // Actualizar/Eliminar cita completa: ADMIN
-                        //.requestMatchers(HttpMethod.PUT, "/appointments/**").hasRole("ADMIN")
-                        //.requestMatchers(HttpMethod.DELETE, "/appointments/**").hasRole("ADMIN")
+                        // .requestMatchers(HttpMethod.PUT, "/appointments/**").hasRole("ADMIN")
+                        // .requestMatchers(HttpMethod.DELETE, "/appointments/**").hasRole("ADMIN")
 
                         // (APOINSTMENS)Citas: restricciones según rol
                         // Accesos para USER
-                        //.requestMatchers(HttpMethod.POST, "/appointments").hasRole("USER")       // Crear cita
-                        //.requestMatchers(HttpMethod.GET, "/appointments/my").hasRole("USER")     // Ver sus citas
+                        // .requestMatchers(HttpMethod.POST, "/appointments").hasRole("USER") // Crear
+                        // cita
+                        // .requestMatchers(HttpMethod.GET, "/appointments/my").hasRole("USER") // Ver
+                        // sus citas
 
                         // Accesos para WORKER
-                        //.requestMatchers(HttpMethod.GET, "/appointments/worker/**").hasRole("WORKER") // Ver citas por trabajador
-                        //.requestMatchers(HttpMethod.PATCH, "/appointments/**/status").hasRole("WORKER") // Actualizar estado
+                        // .requestMatchers(HttpMethod.GET, "/appointments/worker/**").hasRole("WORKER")
+                        // // Ver citas por trabajador
+                        // .requestMatchers(HttpMethod.PATCH,
+                        // "/appointments/**/status").hasRole("WORKER") // Actualizar estado
 
                         // Accesos para ADMIN
-                        //.requestMatchers(HttpMethod.GET, "/appointments").hasRole("ADMIN")       // Listar todas
-                        //.requestMatchers(HttpMethod.PUT, "/appointments/**").hasRole("ADMIN")    // Actualizar completa
-                        //.requestMatchers(HttpMethod.DELETE, "/appointments/**").hasRole("ADMIN") // Eliminar
-                        //.requestMatchers(HttpMethod.PATCH, "/appointments/**/status").hasRole("ADMIN") // También puede cambiar estado
+                        // .requestMatchers(HttpMethod.GET, "/appointments").hasRole("ADMIN") // Listar
+                        // todas
+                        // .requestMatchers(HttpMethod.PUT, "/appointments/**").hasRole("ADMIN") //
+                        // Actualizar completa
+                        // .requestMatchers(HttpMethod.DELETE, "/appointments/**").hasRole("ADMIN") //
+                        // Eliminar
+                        // .requestMatchers(HttpMethod.PATCH,
+                        // "/appointments/**/status").hasRole("ADMIN") // También puede cambiar estado
 
                         // PROMOTIONS
-                        //.requestMatchers(HttpMethod.GET, "/promotions/**").hasAnyRole("USER", "WORKER", "ADMIN") // listar/consultar
-                        //.requestMatchers("/promotions/**").hasRole("ADMIN") // crear/editar/eliminar
-
+                        // .requestMatchers(HttpMethod.GET, "/promotions/**").hasAnyRole("USER",
+                        // "WORKER", "ADMIN") // listar/consultar
+                        // .requestMatchers("/promotions/**").hasRole("ADMIN") // crear/editar/eliminar
 
                         // INVOICE
-                        //.requestMatchers(HttpMethod.POST, "/invoices/**").hasRole("USER") // crear factura
-                        //.requestMatchers(HttpMethod.GET, "/invoices/my/**").hasRole("USER") // listar mis facturas
-                        //.requestMatchers("/invoices/**").hasRole("ADMIN") // actualizar, eliminar, listar todas
+                        // .requestMatchers(HttpMethod.POST, "/invoices/**").hasRole("USER") // crear
+                        // factura
+                        // .requestMatchers(HttpMethod.GET, "/invoices/my/**").hasRole("USER") // listar
+                        // mis facturas
+                        // .requestMatchers("/invoices/**").hasRole("ADMIN") // actualizar, eliminar,
+                        // listar todas
 
                         // Payments
-                        //.requestMatchers(HttpMethod.POST, "/payments").hasRole("USER")
-                        //.requestMatchers(HttpMethod.GET, "/payments/my").hasRole("USER")
-                        //.requestMatchers("/payments/**").hasRole("ADMIN") // GET all, PUT, DELETE
+                        // .requestMatchers(HttpMethod.POST, "/payments").hasRole("USER")
+                        // .requestMatchers(HttpMethod.GET, "/payments/my").hasRole("USER")
+                        // .requestMatchers("/payments/**").hasRole("ADMIN") // GET all, PUT, DELETE
 
                         // Appointments
-                        //.requestMatchers("/appointments/**").authenticated()
+                        // .requestMatchers("/appointments/**").authenticated()
 
-                        .anyRequest().authenticated()
-        )       .httpBasic(Customizer.withDefaults())
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
                 .addFilterBefore(new JwtAuthenticationFilter(jwtUtil, userDetailsService),
                         UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
